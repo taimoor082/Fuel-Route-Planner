f73d25729eca22326f36070a98d14425ed5d5aee
taimoor <se0075@bjs-softsolution.com>
2026-07-20 15:38:44 +0500

    Revert "Fuel Router Planner"
    This reverts commit f45d8fb48b9eb1383799e73cdcb53ec500a87617.

--- routes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-from routes.views import RoutePlanView
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='map.html'), name='map'),
-    path('api/route/', RoutePlanView.as_view(), name='route-plan'),
-]
